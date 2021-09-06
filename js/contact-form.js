5acb204397d294264fb8fbf32c136ffff44f0a5e
Casmon Gordon <casmongordon@Casmons-iMac-WFT-4.local>
2021-09-06 02:58:03 -0400

    initial revision

---
+++ js/contact-form.js
@@ -0,0 +1,72 @@
+/*
+--------------------------------
+Ajax Contact Form
+--------------------------------
++ https://github.com/mehedidb/Ajax_Contact_Form
++ A Simple Ajax Contact Form developed in PHP with HTML5 Form validation.
++ Has a fallback in jQuery for browsers that do not support HTML5 form validation.
++ version 1.0.1
++ Copyright 2016 Mehedi Hasan Nahid
++ Licensed under the MIT license
++ https://github.com/mehedidb/Ajax_Contact_Form
+*/
+
+(function ($, window, document, undefined) {
+    'use strict';
+
+    var $form = $('#contact-form');
+
+    $form.submit(function (e) {
+        // remove the error class
+        $('.form-group').removeClass('has-error');
+        $('.help-block').remove();
+
+        // get the form data
+        var formData = {
+            'name' : document.getElementById("name").value,
+            'email' : document.getElementById("email").value,
+            'subject' : 'PORTFOLIO',
+            'message' : document.getElementById("message").value
+        };
+
+        // process the form
+        $.ajax({
+            type : 'POST',
+            url  : 'process.php',
+            data : formData,
+            dataType : 'json',
+            encode : true
+        }).submit(function (data) {
+            // handle errors
+            if (!data.success) {
+                if (data.errors.name) {
+                    $('#name-field').addClass('has-error');
+                    $('#name-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.name + '</span>');
+                }
+
+                if (data.errors.email) {
+                    $('#email-field').addClass('has-error');
+                    $('#email-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.email + '</span>');
+                }
+
+                if (data.errors.subject) {
+                    $('#subject-field').addClass('has-error');
+                    $('#subject-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.subject + '</span>');
+                }
+
+                if (data.errors.message) {
+                    $('#message-field').addClass('has-error');
+                    $('#message-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.message + '</span>');
+                }
+            } else {
+                // display success message
+                alert(data.message);
+                $form.html('<div class="alert alert-success">' + data.message + '</div>');
+            }
+        }).fail(function (data) {
+            // for debug
+            console.log(data)
+        });
+        e.preventDefault();
+    });
+}(jQuery, window, document));
